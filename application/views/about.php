5477fcc1f739457af8b7cb80b02e773cd768f012
nidhik3 <nidhikatkani3.com>
2024-07-19 17:53:36 +0530

    aboust-start

--- application/views/about.php
+++ application/views/about.php
@@ -5,4 +5,10 @@
             ADMOG Holding</p>
             <p class="bannerp " >Leading the way in responsible exploration, development, and trading of natural resources.</p>
         </div>   
-    </section>
+ </section>
+
+ <section>
+    <div class="container">
+        <p class="p-para">ADMOG Holding Corp. is a leading natural resources holding company dedicated to the exploration, development, and  trading of natural resources. With a strategic focus on the metals sector, we are committed to delivering sustainable value through innovative practices, responsible resource management, and a robust trading platform.</p>
+    </div>
+ </section>
